b4545121f1adb0a989c582f4efce000baaa88596
name-mun <tains9153@gmail.com>
2024-12-06 17:42:58 +0900

    first commit

---
+++ sparta-ios-project-phonebookTests/sparta_ios_project_phonebookTests.swift
@@ -0,0 +1,17 @@
+//
+//  sparta_ios_project_phonebookTests.swift
+//  sparta-ios-project-phonebookTests
+//
+//  Created by mun on 12/6/24.
+//
+
+import Testing
+@testable import sparta_ios_project_phonebook
+
+struct sparta_ios_project_phonebookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ sparta-ios-project-phonebookUITests/sparta_ios_project_phonebookUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  sparta_ios_project_phonebookUITestsLaunchTests.swift
+//  sparta-ios-project-phonebookUITests
+//
+//  Created by mun on 12/6/24.
+//
+
+import XCTest
+
+final class sparta_ios_project_phonebookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
