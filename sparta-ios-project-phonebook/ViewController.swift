a45b795844f17942889e9ee8512b3e860ec0c60b
mun <77603287+name-mun@users.noreply.github.com>
2024-12-09 15:50:25 +0900

    [feature/lv2] Implement Level 2 Features (#4)
    * ✨ Add NavigationController
    
    * ✨ Create `PhoneBookViewController` layout and UI components
    
    * 🐛 Resolve `navigationBar` problem

--- sparta-ios-project-phonebook/ViewController.swift
+++ sparta-ios-project-phonebook/ViewController.swift
@@ -21,11 +21,12 @@ class ViewController: UIViewController {
         return label
     }()
 
-    let addButton: UIButton = {
+    lazy var addButton: UIButton = {
         let button = UIButton()
         button.setTitle("추가", for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 20)
         button.setTitleColor(.gray, for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         return button
     }()
 
@@ -39,6 +40,10 @@ class ViewController: UIViewController {
     }()
 
     // MARK: - 초기 설정
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,7 +68,7 @@ class ViewController: UIViewController {
         }
 
         addButton.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.top)
+            $0.centerY.equalTo(titleLabel.snp.centerY)
             $0.trailing.equalTo(safeArea.snp.trailing).inset(20)
             $0.height.equalTo(25)
         }
@@ -99,6 +104,16 @@ extension ViewController: UITableViewDelegate {
     // tableView 행 높이 설정
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
+    }
+}
+
+// MARK: - 버튼 클리식 실행
+
+extension ViewController {
+
+    @objc
+    func buttonTapped() {
+        self.navigationController?.pushViewController(PhoneBookViewController(), animated: true)
     }
 }
 
