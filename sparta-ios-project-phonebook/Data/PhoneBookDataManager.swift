f24806270ee09a376eb0c252a5d980853f3738c6
name-mun <tains9153@gmail.com>
2024-12-11 16:39:35 +0900

    ✨ Add feature to update `PhoneBookDataModel` in `CoreData`

--- sparta-ios-project-phonebook/Data/PhoneBookDataManager.swift
+++ sparta-ios-project-phonebook/Data/PhoneBookDataManager.swift
@@ -50,6 +50,8 @@ extension PhoneBookDataManager {
     func createData(name: String, phoneNumber: String, profileImage: String) {
         guard let entity = NSEntityDescription.entity(forEntityName: PhoneBook.className, in: context) else { return }
         let newPhoneBook = NSManagedObject(entity: entity, insertInto: context)
+
+        newPhoneBook.setValue(UUID(), forKey: PhoneBook.Key.id)
         newPhoneBook.setValue(name, forKey: PhoneBook.Key.name)
         newPhoneBook.setValue(phoneNumber, forKey: PhoneBook.Key.phoneNumber)
         newPhoneBook.setValue(profileImage, forKey: PhoneBook.Key.profileImage)
@@ -77,8 +79,26 @@ extension PhoneBookDataManager {
     }
 
     // 데이터 업데이트
-    func updateData() {
-        // TODO: -
+    func updateData(id: UUID, name: String, phoneNumber: String, profileImage: String) {
+        let fetchRequest = PhoneBook.fetchRequest()
+        // id 값으로 데이터 검색
+        fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+
+        do {
+            let result = try self.context.fetch(fetchRequest)
+
+            for data in result as [NSManagedObject] {
+                data.setValue(name, forKey: PhoneBook.Key.name)
+                data.setValue(phoneNumber, forKey: PhoneBook.Key.phoneNumber)
+                data.setValue(profileImage, forKey: PhoneBook.Key.profileImage)
+            }
+
+            try self.context.save()
+
+            print("데이터 수정 성공")
+        } catch {
+            print("데이터 수정 실패")
+        }
     }
 
     // 데이터 삭제
